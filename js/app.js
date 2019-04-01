1c84a4a4ed80e68ebe225bc7060c22dfc3a01358
Administrator <dean_lin@ubiqconn.com>
2019-04-01 14:14:42 +0800

    init

--- js/app.js
+++ js/app.js
@@ -11,37 +11,54 @@ var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
-var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
-pauseButton.addEventListener("click", pauseRecording);
+var recordStatus = 'stop'
+$(document).ready(function () {
+	// 先詢問音訊
+	created ()
+	// 偵測磁場
+	let sensor = new Magnetometer();
+	let sensorValue = 0;
+	sensor.start();
+	sensor.onreading = () => {
+		sensorValue = Math.round(Math.abs(sensor.x) + Math.abs(sensor.y) + Math.abs(sensor.z))
+		if (sensorValue > 200 && recordStatus === 'stop') {
+			recordStatus = 'start'
+			console.log('開始錄音')
+			startRecording()
+		}
+		if (sensorValue < 200 && recordStatus === 'start') {
+			recordStatus = 'end'
+			console.log('結束錄音')
+			stopRecording()
+		}
+	};
+	sensor.onerror = event => console.log(event.error.name, event.error.message);
+});
 
-function startRecording() {
-	console.log("recordButton clicked");
 
+function created () {
 	/*
 		Simple constraints object, for more advanced audio features see
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
-    
-    var constraints = { audio: true, video:false }
 
- 	/*
-    	Disable the record button until we get a success or fail from getUserMedia() 
-	*/
+	var constraints = { audio: true, video: false }
 
-	recordButton.disabled = true;
-	stopButton.disabled = false;
-	pauseButton.disabled = false
+	/*
+	  Disable the record button until we get a success or fail from getUserMedia() 
+  */
 
+	
 	/*
     	We're using the standard promise based getUserMedia() 
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
-
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+	if(navigator.getUserMedia){}
+	navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
 		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
 		/*
@@ -53,11 +70,11 @@ function startRecording() {
 		audioContext = new AudioContext();
 
 		//update the format 
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+		document.getElementById("formats").innerHTML = "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz"
 
 		/*  assign to gumStream for later use  */
 		gumStream = stream;
-		
+
 		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
 
@@ -65,46 +82,33 @@ function startRecording() {
 			Create the Recorder object and configure to record mono sound (1 channel)
 			Recording 2 channels  will double the file size
 		*/
-		rec = new Recorder(input,{numChannels:1})
-
-		//start the recording process
-		rec.record()
-
-		console.log("Recording started");
-
-	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
-    	recordButton.disabled = false;
-    	stopButton.disabled = true;
-    	pauseButton.disabled = true
+		rec = new Recorder(input, { numChannels: 1 })
+	}).catch(function (err) {
+		//enable the record button if getUserMedia() fails
+		recordButton.disabled = false;
+		stopButton.disabled = true;
 	});
 }
 
-function pauseRecording(){
-	console.log("pauseButton clicked rec.recording=",rec.recording );
-	if (rec.recording){
-		//pause
-		rec.stop();
-		pauseButton.innerHTML="Resume";
-	}else{
-		//resume
-		rec.record()
-		pauseButton.innerHTML="Pause";
+function startRecording () {
+	//start the recording process
+	recordButton.disabled = true;
+	stopButton.disabled = false;
 
-	}
+	rec.record()
+
+	console.log("Recording started");
 }
 
-function stopRecording() {
+
+function stopRecording () {
 	console.log("stopButton clicked");
 
 	//disable the stop button, enable the record too allow for new recordings
 	stopButton.disabled = true;
 	recordButton.disabled = false;
-	pauseButton.disabled = true;
 
-	//reset button just in case the recording is stopped while paused
-	pauseButton.innerHTML="Pause";
-	
+
 	//tell the recorder to stop the recording
 	rec.stop();
 
@@ -115,8 +119,8 @@ function stopRecording() {
 	rec.exportWAV(createDownloadLink);
 }
 
-function createDownloadLink(blob) {
-	
+function createDownloadLink (blob) {
+
 	var url = URL.createObjectURL(blob);
 	var au = document.createElement('audio');
 	var li = document.createElement('li');
@@ -131,35 +135,35 @@ function createDownloadLink(blob) {
 
 	//save to disk link
 	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.download = filename + ".wav"; //download forces the browser to donwload the file using the  filename
 	link.innerHTML = "Save to disk";
 
 	//add the new audio element to li
 	li.appendChild(au);
-	
+
 	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
+	li.appendChild(document.createTextNode(filename + ".wav "))
 
 	//add the save to disk link to li
 	li.appendChild(link);
-	
+
 	//upload link
 	var upload = document.createElement('a');
-	upload.href="#";
+	upload.href = "#";
 	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		  var xhr=new XMLHttpRequest();
-		  xhr.onload=function(e) {
-		      if(this.readyState === 4) {
-		          console.log("Server returned: ",e.target.responseText);
-		      }
-		  };
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
-		  xhr.send(fd);
+	upload.addEventListener("click", function (event) {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function (e) {
+			if (this.readyState === 4) {
+				console.log("Server returned: ", e.target.responseText);
+			}
+		};
+		var fd = new FormData();
+		fd.append("audio_data", blob, filename);
+		xhr.open("POST", "upload.php", true);
+		xhr.send(fd);
 	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
+	li.appendChild(document.createTextNode(" "))//add a space in between
 	li.appendChild(upload)//add the upload link to li
 
 	//add the li element to the ol
